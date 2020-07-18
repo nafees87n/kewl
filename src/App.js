4c2a63248a7062f39b27d288d644b5703b4e4c47
Nafees Nehar <nafees87n@gmail.com>
2020-07-18 19:59:59 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import One from "./Components/one";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route exact={true} path="/" component={One} />
+        {/* <Route
+          exact={true}
+          path="/two"
+          component={two}
+          />
+          <Route
+          exact={true}
+          path="/three"
+          component={three}
+          /> */}
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/one.js
@@ -0,0 +1,11 @@
+import React,{ Component } from "react";
+
+class One extends Component{
+    render(){
+        return(
+            <h1>Test Homepage</h1>
+        )
+    }
+}
+
+export default One;
