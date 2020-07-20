fc84ce5227de5e873c8879645e5978fc172cfc37
Nafees Nehar <nafees87n@gmail.com>
2020-07-20 20:24:54 +0530

    bootstrap added

--- src/Components/one.js
+++ src/Components/one.js
@@ -1,11 +1,28 @@
-import React,{ Component } from "react";
-
-class One extends Component{
-    render(){
-        return(
-            <h1>Test Homepage</h1>
-        )
-    }
+import React, { Component } from "react";
+import {
+  Navbar,
+  NavLink,
+  NavItem,
+  Nav,
+  NavbarToggler,
+  NavbarBrand,
+  Collapse,
+  Button,
+} from "reactstrap";
+class One extends Component {
+  render() {
+    return (
+      <>
+        <Navbar dark>
+          <div className="container">
+            <Nav navbar className="ml-auto" style={{color:"white"}}>
+              <NavItem>Components</NavItem>
+            </Nav>
+          </div>
+        </Navbar>
+        </>
+    );
+  }
 }
 
 export default One;
