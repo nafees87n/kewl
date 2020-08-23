5d45aa6b352df531338ba6990df8d782251c40ae
Nafees Nehar <nafees87n@gmail.com>
2020-08-23 11:02:58 +0530

    homepage fix shinde reverted

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Two from './Components/page-2/two.js';
-// import App from './App';
+// import Two from './Components/page-2/two.js';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     
-    {/* <App /> */}
-    <Two />
+    <App />
+    {/* <Two /> */}
   
   </React.StrictMode>,
   document.getElementById('root')
