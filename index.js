042db83f11e75f90ee3ef115a62655ed29fa936e
Nafees Nehar <nafees87n@gmail.com>
2020-11-06 12:33:15 +0530

    fix

--- index.js
+++ index.js
@@ -17,10 +17,12 @@ app.use('/login', login);
 
 app.use(express.static('client/build'));
 
-app.get('*/', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-  });
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
+app.get('/login/redirect', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
 const port = process.env.PORT || 5000;
 // eslint-disable-next-line no-console
 app.listen(port, console.log(`Server started at port ${port}`));
