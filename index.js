565aa6f510b03e11b4ac6b620f815f5d6b81f3af
Nafees Nehar <nafees87n@gmail.com>
2020-11-06 12:53:54 +0530

    fix: redirect

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use('/login', login);
 
 app.use(express.static('client/build'));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
 app.get('/login/redirect', (req, res) => {
--- server/routes/login.js
+++ server/routes/login.js
@@ -23,7 +23,7 @@ router.get('/redirect', async (req, res) => {
   // console.log(token);
   // console.log("HII");
   res.cookie('chatToken', idToken);
-  res.redirect(200,'https://tranquil-chamber-48344.herokuapp.com/login/redirect');
+  res.redirect(200,'https://tranquil-chamber-48344.herokuapp.com/dashboard');
   // res.send({ token: idToken });
 });
 // app.post('/form', (req, res) => {
