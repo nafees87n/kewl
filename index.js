5098102c10875cf5ee25d157d5cc91b9347e5f7f
Nafees Nehar <nafees87n@gmail.com>
2020-11-06 11:13:27 +0530

    fix:redirect

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.use(cors());
 app.use(cookieParser());
 app.use('/login', login);
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static('client/build'));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build', 'index.html'));
   });
 
 const port = process.env.PORT || 5000;
--- server/routes/login.js
+++ server/routes/login.js
@@ -7,13 +7,12 @@ const axios = require('axios').default;
 
 router.get('/redirect', async (req, res) => {
   // console.log("REQ CODE", req.query.code); //what is this code
-  const currUrl=window.location.href;
   const idToken = await axios
     .post('https://oauth2.googleapis.com/token', {
       code: req.query.code,
       client_id: process.env.CLIENTID,
       client_secret: process.env.CLIENT_SECRET,
-      redirect_uri: `http://https://tranquil-chamber-48344.herokuapp.com//login/redirect`,
+      redirect_uri: `http://tranquil-chamber-48344.herokuapp.com/login/redirect`,
       grant_type: 'authorization_code'
     })
     .then((back) => back.data.id_token);
